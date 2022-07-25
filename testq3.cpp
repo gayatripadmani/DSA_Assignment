29542795532958fcb84a4b6fd5cd57bd73e5da8b
Gayatri Padmani <67087179+gayatripadmani@users.noreply.github.com>
2022-07-25 21:38:31 +0530

    Update testq3.cpp

--- testq3.cpp
+++ testq3.cpp
@@ -3,19 +3,38 @@ using namespace std;
 
 int main()
 {
-    int arr[100], size, i, sum = 0;
-    
-    cout << endl << "Enter array size: ";
-    cin >> size;
+    int a[10], b[10], n, i, j;
+    cout << endl << "Enter how many numbers you want to calculate sum of digits: ";
+    cin >> n;
 
-    cout << endl << "Enter array elements: ";
-    for(i = 0; i < size; i++)
-          cin >> arr[i];
-          
-    for(i = 0; i < size; i++)
-          sum = sum + arr[i]; 
+    for(i=0;i<n;i++)
+    {
+        cout << endl << "Enter Number: ";
+        cin >> a[i];
+
+        int sum=0, temp=a[i], rev;
+
+        while(temp>0)
+        {
+            rev=temp%10;
+            sum=sum+rev;
+            temp/=10;
+        }
+
+        b[i] = sum;
+    }
+
+    for(i=0;i<n;i++)
+    { 
+        cout << endl << a[i];
+    }
+
+    cout<<endl<<"Sum of array: ";
     
-    cout << endl << "Sum of the array = " << sum;
-    
+    for(i=0;i<n;i++)
+    {
+        cout << endl << b[i];
+    }
+
     return 0;
 }
